717376ba94bccaf372f36ae704801387da934868
Arijit Das <arijit10das2002@gmail.com>
2022-07-31 22:32:30 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import Login from "./components/LoginComponent/Login";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { Dashboard } from './components/DashboardComponent/Dashboard';
+
 
 function App() {
   return (
-    <Login />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/LoginComponent/Login.jsx
+++ src/components/LoginComponent/Login.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import { Redirect} from 'react-router-dom';
 import "./login.css";
 import { useState } from 'react';
 import logo from "../../assets/logo8.png";
-import { Dashboard } from "../DashboardComponent/Dashboard";
  
 function App() {
   const [name, setName] = useState('');
@@ -47,7 +47,7 @@ function App() {
           </>
         ) : (
           <>
-            <Dashboard />
+          <Redirect to="/dashboard" />
           </>
         )}
       </div>
